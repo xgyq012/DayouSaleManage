88bacbcbf687058f0b03c67f95cc7e9c0eb7ec7f
DESKTOP-JSFA1I0\hellow <xgyqapple@qq.com>
2017-01-07 16:04:52 +0800

    修改了 主要代码

--- src/main/java/com/dy/sale/repository/test.java
+++
@@ -1,7 +0,0 @@
-package com.dy.repository;
-
-/**
- * Created by hellow on 2016/12/8.
- */
-public class test {
-}
--- src/main/java/com/dy/sale/service/BaseService.java
+++
@@ -1,7 +0,0 @@
-package com.dy.sale.service;
-
-
-public interface BaseService {
-
-
-}
